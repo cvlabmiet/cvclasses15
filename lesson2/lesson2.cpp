874dcc89c71003f47b5daa59f15b0551fbb87d30
stepansidorov <sidorov_stepan@mail.ru>
2015-10-07 00:04:51 +0300

    lesson2 hometask

--- lesson2/lesson2.cpp
+++ lesson2/lesson2.cpp
@@ -1,16 +1,19 @@
 ///@File: main.cpp
 ///@Brief: Contains implementation of entry point of the application
-///@Author: Svetlana Kuksova
-///@Date: 21 September 2015
+///@Author: Stepan Sidorov
+///@Date: 06 October 2015
 
 #include "stdafx.h"
 
 #include "CannyEdgeDetector.h"
+#include "HarrisCornerDetector.h"
 
 ///@brief Entry point
 int main(int argc, char** argv)
 {
-	CannyEdgeDetector CannyDemo;
-	CannyDemo.Show(cv::String(argv[1]));
-	return 0;
+   CannyEdgeDetector cannyDemo;
+   HarrisCornerDetector harrisDemo;
+   cannyDemo.Show(cv::String(argv[1]));
+//   harrisDemo.Show(cv::String(argv[1]));
+   return 0;
 }
